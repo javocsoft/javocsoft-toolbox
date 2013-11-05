5d1adb80303df2145baefc393d5690cf9fc33612
JavocSoft <javocsoft@gmail.com>
2013-11-05 22:42:39 +0100

    CMT Short Numbers informative helper class update

--- src/es/javocsoft/android/lib/toolbox/sms/CMTShortNumberInformation.java
+++ src/es/javocsoft/android/lib/toolbox/sms/CMTShortNumberInformation.java
@@ -4,14 +4,16 @@ import java.util.Locale;
 
 
 /**
- * Información.<br><br>
+ * Information.<br><br>
  * 
- * Ver <a href="http://www.cmt.es/descarga-ficheros-numeracion">Descarga de numeraciones</a>
+ * See <a href="http://www.cmt.es/descarga-ficheros-numeracion">Descarga de numeraciones</a>
  * 
- * Ver <a href="http://www.altiria.com/regulacion-normativa-sms-premium/">Normativa 2008 SMS Premium</a><br> 
- * Ver <a href="http://cnmcblog.es/2009/09/01/su-numero-gracias/">Más Información</a><br>
- * Ver <a href="http://www.cmt.es/instrucciones">CMT</a><br><br>
+ * See <a href="http://www.altiria.com/regulacion-normativa-sms-premium/">Normative 2008 SMS Premium</a><br> 
+ * See <a href="http://cnmcblog.es/2009/09/01/su-numero-gracias/">More information</a><br>
+ * See <a href="http://www.cmt.es/instrucciones">CMT Numeration related information</a><br><br>
  * 
+ * Note from CMT web page:
+ * "
  *	Servicios SMS/MMS: aquellos servicios basados en mensajes de texto y multimedia, entre los que se incluyen:<br>
  *		Servicios SMS/MMS gratuitos: aquellos códigos numéricos de seis dígitos de que empiezan por 205 y 207.<br>
  *		Servicios SMS/MMS con tarificación normal: aquellos códigos numéricos de seis dígitos de que empiezan por 215 y 217.<br>
@@ -20,11 +22,12 @@ import java.util.Locale;
  *			b) Precio entre 1,2 y 6 euros:Códigos de cinco dígitos que empiezan por 35 y 37.<br>
  *			c) Suscripción con precio inferior o igual a 1,2 euros: Códigos de seis dígitos que empiezan por 795 y 797.<br>
  *			d) Adultos con precio inferior o igual a 6 euros: Códigos de seis dígitos que empiezan por 995 y 997.<br><br>
+ * "
  * 
  * @author JavocSoft 2013
  * @version 1.0<br>
- * $Rev: 331 $<br>
- * $LastChangedDate: 2013-11-01 14:37:01 +0100 (Fri, 01 Nov 2013) $<br>
+ * $Rev: 349 $<br>
+ * $LastChangedDate: 2013-11-02 14:09:43 +0100 (Sat, 02 Nov 2013) $<br>
  * $LastChangedBy: jgonzalez $
  *
  */
@@ -45,7 +48,7 @@ public class CMTShortNumberInformation {
 	public static final String SMS_MMS_PREMIUM_5DIGITS__UPTO_1_2_E_STARTING_280 = "280";
 	
 	public static final String SMS_MMS_PREMIUM_6DIGITS__UPTO_1_2_E_STARTING_795 = "795";
-	public static final String SMS_MMS_PREMIUM_6DIGITS__UPTO_1_2_E_STARTING_27 = "797";
+	public static final String SMS_MMS_PREMIUM_6DIGITS__UPTO_1_2_E_STARTING_797 = "797";
 	
 	public static final String SMS_MMS_PREMIUM_5DIGITS__FROM_1_2_TO_6_E_STARTING_35 = "35";
 	public static final String SMS_MMS_PREMIUM_5DIGITS__FROM_1_2_TO_6_E_STARTING_37 = "37";
@@ -53,6 +56,9 @@ public class CMTShortNumberInformation {
 	public static final String SMS_MMS_PREMIUM_6DIGITS__UPTO_6_E_STARTING_995 = "995";
 	public static final String SMS_MMS_PREMIUM_6DIGITS__UPTO_6_E_STARTING_997 = "997";
 	
+	public static enum TARIFICATION {FREE, NORMAL, PREMIUM, UNKNOWN};
+	
+	public static enum PRICE {UPTO_1_2E, UPTO_6E};
 	
 	public enum CMTSMSMMSShortNumberIdentifier {
 		SMS_MMS_FREE_6DIGITS__STARTING_205(205),
@@ -63,7 +69,7 @@ public class CMTShortNumberInformation {
 		SMS_MMS_PREMIUM_5DIGITS__UPTO_1_2_E_STARTING_27(27),
 		SMS_MMS_PREMIUM_5DIGITS__UPTO_1_2_E_STARTING_280(280),
 		SMS_MMS_PREMIUM_6DIGITS__UPTO_1_2_E_STARTING_795(795),
-		SMS_MMS_PREMIUM_6DIGITS__UPTO_1_2_E_STARTING_27(27),
+		SMS_MMS_PREMIUM_6DIGITS__UPTO_1_2_E_STARTING_797(797),
 		SMS_MMS_PREMIUM_5DIGITS__FROM_1_2_TO_6_E_STARTING_35(35),
 		SMS_MMS_PREMIUM_5DIGITS__FROM_1_2_TO_6_E_STARTING_37(37),
 		SMS_MMS_PREMIUM_6DIGITS__UPTO_6_E_STARTING_995(995),
@@ -79,57 +85,203 @@ public class CMTShortNumberInformation {
 		 public int getValue() {
 		   return value;
 		 }
+	};
+
+	
+	/**
+	  * Returns TRUE if the number is recognized by
+	  * the CMT.
+	  * 
+	  * @param number
+	  * @return
+	  */
+	 public static boolean isRecognized(String number){
+		 boolean res = false;
+		
+		 if(number.length()==5 || number.length()==6){
 		 
-		 /**
-		  * Returns TRUE if the number is recognized by
-		  * the CMT.
-		  * 
-		  * @param number
-		  * @return
-		  */
-		 public static boolean isRecognized(String number){
-			 boolean res = false;
-			
-			 if(number.length()==5 || number.length()==6){
+			 String id3digits = number.substring(0, 3);
+			 String id2digits = number.substring(0, 2);
 			 
-				 String id3digits = number.substring(0, 3);
-				 String id2digits = number.substring(0, 2);
-				 
-				 try{
-					 for (CMTSMSMMSShortNumberIdentifier item : CMTSMSMMSShortNumberIdentifier.values()) {
-						  if(item.getValue()==Integer.valueOf(id3digits)){
+			 try{
+				 for (CMTSMSMMSShortNumberIdentifier item : CMTSMSMMSShortNumberIdentifier.values()) {
+					  if(item.getValue()==Integer.valueOf(id3digits)){
+						  res = true;
+						  break;
+					  }
+				 }					 
+			 }catch(Exception e){}
+			 
+			 try{
+				 for (CMTSMSMMSShortNumberIdentifier item : CMTSMSMMSShortNumberIdentifier.values()) {
+					  if(item.getValue()==Integer.valueOf(id2digits)){
+						  res = true;
+						  break;
+					  }
+				 }					 
+			 }catch(Exception e){}
+		 }				 
+		 
+		 return res;
+	 }
+	
+	/**
+	  * Tells if a text is a subscription text.
+	  * 
+	  * @param text
+	  * @return
+	  */
+	 public static boolean isSubscription(String text){			 
+		 if(text.toUpperCase(Locale.getDefault()).contains(SMS_MMS_SUBSCRIPTION_KEYWORD_ES)){
+			 return true;
+		 }else{
+			 return false;
+		 }
+	 }
+	 
+	 /**
+	  * 
+	  * @param text
+	  * @return
+	  */
+	 public static boolean isFromASubscriptionShortNumber(String number){			 
+		 boolean res = false;
+			
+		 if(number.length()==5 || number.length()==6){
+		 
+			 String id3digits = number.substring(0, 3);
+			 
+			 try{
+				 for (CMTSMSMMSShortNumberIdentifier item : CMTSMSMMSShortNumberIdentifier.values()) {
+					  if(item.getValue()==Integer.valueOf(id3digits)){
+						  if(item == CMTSMSMMSShortNumberIdentifier.SMS_MMS_PREMIUM_6DIGITS__UPTO_1_2_E_STARTING_795 ||
+							 item == CMTSMSMMSShortNumberIdentifier.SMS_MMS_PREMIUM_6DIGITS__UPTO_1_2_E_STARTING_797){
+							  //These are the oficial short numbers for a subscription service.
+							  res = true;
+							  break;
+						  }else if(item == CMTSMSMMSShortNumberIdentifier.SMS_MMS_PREMIUM_6DIGITS__UPTO_6_E_STARTING_995 ||
+								   item == CMTSMSMMSShortNumberIdentifier.SMS_MMS_PREMIUM_6DIGITS__UPTO_6_E_STARTING_997){
+							  //There are companies that for adult content use these prefixes to deliver subscription contents.
 							  res = true;
 							  break;
 						  }
-					 }					 
-				 }catch(Exception e){}
-				 
+					  }
+				 }					 
+			 }catch(Exception e){}
+		 }				 
+		 
+		 return res;
+	 }	 
+	
+	/**
+	 * Gets the tarification type for a given short number.
+	 * 
+	 * @param number
+	 * @return	TARIFICATION
+	 */
+	 public static TARIFICATION getTarificationType(String number){
+		 TARIFICATION res = null;
+		 
+		 if(number.length()==5 || number.length()==6){
+			 
+			 String id3digits = number.substring(0, 3);
+			 String id2digits = number.substring(0, 2);
+			 
+			//Three digits ones
+			 try{
+				 for (CMTSMSMMSShortNumberIdentifier item : CMTSMSMMSShortNumberIdentifier.values()) {
+					  if(item.getValue()==Integer.valueOf(id3digits)){
+						  res = askForTarification(item);
+						  break;					  
+					  }
+				 }					 
+			 }catch(Exception e){}
+			 
+			 if(res==null){
+				 //Two digits ones
 				 try{
 					 for (CMTSMSMMSShortNumberIdentifier item : CMTSMSMMSShortNumberIdentifier.values()) {
 						  if(item.getValue()==Integer.valueOf(id2digits)){
-							  res = true;
+							  res = askForTarification(item);
 							  break;
 						  }
-					 }					 
+					 }	 
 				 }catch(Exception e){}
-			 }				 
+			 }
+		 }		 
+		 
+		 return res;
+	 }
+	 
+	 /**
+	  * Gets the price of the given short premium number.
+	  * 
+	  * @param number
+	  * @return PRICE
+	  */
+	 public static PRICE getPremiumPrice(String number) {
+		 PRICE res = null;
+		 
+		 if(number.length()==5 || number.length()==6){
 			 
-			 return res;
+			 String id3digits = number.substring(0, 3);
+			 String id2digits = number.substring(0, 2);
+			 
+			//Three digits ones
+			 try{
+				 for (CMTSMSMMSShortNumberIdentifier item : CMTSMSMMSShortNumberIdentifier.values()) {
+					  if(item.getValue()==Integer.valueOf(id3digits)){
+						  res = askForPrice(item);
+						  break;					  
+					  }
+				 }					 
+			 }catch(Exception e){}
+			 
+			 if(res==null){
+				 //Two digits ones
+				 try{
+					 for (CMTSMSMMSShortNumberIdentifier item : CMTSMSMMSShortNumberIdentifier.values()) {
+						  if(item.getValue()==Integer.valueOf(id2digits)){
+							  res = askForPrice(item);
+							  break;
+						  }
+					 }	 
+				 }catch(Exception e){}
+			 }
+		 }			 
+		 
+		 return res;
+	 }
+	
+	 
+	 
+	//AUXILIAR
+	 
+	 private static TARIFICATION askForTarification(CMTSMSMMSShortNumberIdentifier item){
+		 TARIFICATION res = null;
+		 
+		 if(item.name().contains("FREE")){
+			 res = TARIFICATION.FREE;				  
+		 }else if(item.name().contains("NORMAL")){
+			 res = TARIFICATION.NORMAL;				  
+		 }else if(item.name().contains("PREMIUM")){
+			 res = TARIFICATION.PREMIUM;				  
+		 }else{
+			 res = TARIFICATION.UNKNOWN;				  
+		 }	
+		 
+		 return res;
+	 }
+	 
+	 private static PRICE askForPrice(CMTSMSMMSShortNumberIdentifier item){
+		 PRICE res = null;
+		 
+		 if(item.name().contains("6_E")){
+			 res = PRICE.UPTO_6E;				  
+		 }else{
+			 res = PRICE.UPTO_1_2E;				  
 		 }
 		 
-		 /**
-		  * Tells if a text is a subscription text.
-		  * 
-		  * @param text
-		  * @return
-		  */
-		 public static boolean isSubscription(String text){			 
-			 if(text.toUpperCase(Locale.getDefault()).contains(SMS_MMS_SUBSCRIPTION_KEYWORD_ES)){
-				 return true;
-			 }else{
-				 return false;
-			 }
-		 }
-	};
-
+		 return res;
+	 }
 }
