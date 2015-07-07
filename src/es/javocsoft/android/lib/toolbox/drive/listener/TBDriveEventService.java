caef719bcb48d71d85cdc43f1d267056831f0a64
JavocSoft <javocsoft@gmail.com>
2015-07-07 17:43:06 +0200

    Added license text to some java files
    :)

--- src/es/javocsoft/android/lib/toolbox/drive/listener/TBDriveEventService.java
+++ src/es/javocsoft/android/lib/toolbox/drive/listener/TBDriveEventService.java
@@ -1,3 +1,24 @@
+/*
+ * Copyright (C) 2010-2015 - JavocSoft - Javier Gonzalez Serrano
+ * http://javocsoft.es/proyectos/code-libs/android/javocsoft-toolbox-android-library
+ * 
+ * This file is part of JavocSoft Android Toolbox library.
+ *
+ * JavocSoft Android Toolbox library is free software: you can redistribute it 
+ * and/or modify it under the terms of the GNU General Public License as 
+ * published by the Free Software Foundation, either version 3 of the License, 
+ * or (at your option) any later version.
+ *
+ * JavocSoft Android Toolbox library is distributed in the hope that it will be 
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General 
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with JavocSoft Android Toolbox library.  If not, see 
+ * <http://www.gnu.org/licenses/>.
+ * 
+ */
 package es.javocsoft.android.lib.toolbox.drive.listener;
 
 import java.io.InputStream;
